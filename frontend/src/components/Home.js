e3590bcd0c09efea14ccb644fa6c03121c27f39a
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 00:39:53 -0400

    Updated Home component to display user-specific information post-login and enhanced styling.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
+
 
 function Home({ isLoggedIn }) {
+  console.log("Is Logged In:", isLoggedIn);
+  const username = localStorage.getItem('username'); // Assuming you store the username in local storage
+
   return (
     <div className="home">
       <h1>Jobly</h1>
       {!isLoggedIn ? (
-        <div>
+        <div className="login-signup-options">
           <Link to="/login">Log In</Link>
           <span> or </span>
           <Link to="/signup">Sign Up</Link>
         </div>
       ) : (
-        <h2>Welcome Back!</h2>
+        <h2>Welcome Back, {username}!</h2> // Displaying the username
       )}
     </div>
   );

a217b7710c7f8c5bbd0c5d494bfcc5f5114721b9
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 00:44:06 -0400

    Enhanced login form to handle user authentication and provide feedback on login status.

--- frontend/src/components/User/LoginForm.js
+++ frontend/src/components/User/LoginForm.js
@@ -2,34 +2,56 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../helpers/api';
 
-function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+function LoginForm({ onLogin }) {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(null);
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    console.log("Updated formData:", formData);
+  };
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      // Perform login logic using API helper
-      await api.login(username, password);
-      navigate('/dashboard'); // Navigate to the dashboard or another route after successful login
+      await api.login(formData);  // Pass the entire formData object
+      setMessage('Login successful! Redirecting to dashboard...');
+      onLogin();
+      navigate('/Home');  // Redirect to the Home page after successful login
     } catch (error) {
       console.error("Login Error:", error);
-      // Handle login error
+      setMessage('Login failed. Please try again.');
+    } finally {
+      setLoading(false);
     }
-  };
+};
+
+
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" name="username" value={formData.username} onChange={handleChange} />
       </label>
       <label>
         Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" name="password" value={formData.password} onChange={handleChange} />
       </label>
-      <button type="submit">Login</button>
+      <button type="submit" disabled={loading}>Login</button>
+      {message && <p>{message}</p>}
+      {loading && <p>Loading...</p>}
     </form>
   );
 }

6cdd9fda5b70d152ba04e627cc5f3bfbc363643d
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 00:40:33 -0400

    Refactored routing logic to handle user authentication and navigation redirects.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import LoginForm from './components/User/LoginForm';
 import SignupForm from './components/User/SignupForm';
 import ProfileForm from './components/User/ProfileForm';
 import NotFound from './components/NotFound';
-import Home from './components/Home'; // Import the Home component
+import Home from './components/Home';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const renderRoot = () => {
+    return isLoggedIn ? <Navigate to="/Home" replace /> : <LoginForm onLogin={handleLogin} />;
+  };
+
   return (
     <Router>
-      <Navigation isLoggedIn={isLoggedIn} />
+      <Navigation isLoggedIn={isLoggedIn} onLogout={handleLogout} />
       <Routes>
-        <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} /> {/* Add the Home route */}
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+        <Route path="/signup" element={<SignupForm onSignup={handleLogin} />} />
         <Route path="/profile" element={<ProfileForm />} />
+        <Route path="/Home" element={<Home isLoggedIn={isLoggedIn} />} />
+        <Route path="/" element={renderRoot()} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>

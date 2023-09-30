47cbfbe179727a0817fa384323935bf03def2259
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 00:43:18 -0400

    Optimized navigation bar to dynamically display links based on user's login status.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,10 +1,10 @@
-// src/components/Navigation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navigation({ isLoggedIn, onLogout }) {
   return (
     <nav>
+      <Link to="/Home">Home</Link> {/* Added a link to the Home page with the title "Jobly" */}
       {isLoggedIn ? (
         <>
           <Link to="/companies">Companies</Link>

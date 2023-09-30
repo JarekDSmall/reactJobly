4a98bff24002777144d2bdaa5fcf35b40863fbfe
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 00:41:49 -0400

    mproved API request handling, added error checks for login credentials.

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -20,56 +20,54 @@ class JoblyApi {
       let message = err.response ? err.response.data.error.message : "API Error";
       throw Array.isArray(message) ? message : [message];
     }
-    
   }
 
   // Individual API routes
 
-  /** Get details on a company by handle. */
   static async getCompany(handle) {
     let res = await this.request(`companies/${handle}`);
     return res.company;
   }
 
-  /** Get the list of all companies. */
   static async getCompanies() {
     let res = await this.request(`companies`);
     return res.companies;
   }
 
-  /** Get details on a job by id. */
   static async getJob(id) {
     let res = await this.request(`jobs/${id}`);
     return res.job;
   }
 
-  /** Get the list of all jobs. */
   static async getJobs() {
     let res = await this.request(`jobs`);
     return res.jobs;
   }
 
-  /** User login. */
   static async login(credentials) {
-    let res = await this.request(`login`, credentials, "post");
-    JoblyApi.token = res.token; // Set the token
-    return res.token;
-  }
+    // Check if both username and password are present in the credentials
+    if (!credentials.username || !credentials.password) {
+      console.error("Missing username or password in credentials:", credentials);
+      throw new Error("Missing username or password");
+    }
   
+    console.log("Sending credentials:", credentials);
+    let res = await this.request(`auth/token`, credentials, "post");
+    JoblyApi.token = res.token;
+    localStorage.setItem('token', res.token);
+    return res.token;
+}
 
-  /** User registration. */
   static async register(userData) {
-    let res = await this.request(`auth/register`, userData, "post"); // Updated the endpoint here
+    let res = await this.request(`auth/register`, userData, "post");
     this.token = res.token;
     localStorage.setItem('token', res.token);
     return res.token;
   }
 
-  // ... add more methods as needed ...
+
 }
 
-// For now, put token ("testuser" / "password" on class)
-// JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
 JoblyApi.token = localStorage.getItem('token') || null;
 
 export default JoblyApi;

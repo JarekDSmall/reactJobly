36f32df83a77a90fab4677c7845d5fd64a088b39
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 00:53:07 -0400

    Refined database connection settings and added error handling for better stability

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,32 @@
 "use strict";
-/** Database setup for jobly. */
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-let db;
+const dbConfig = {
+  host: "localhost",
+  port: 5432,
+  database: getDatabaseUri(),
+  // user: "postgres",  // or the user you want to use
+  // password: "YOUR_PASSWORD_HERE"  // replace with your postgres user's password
+};
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
+const db = new Client(process.env.NODE_ENV === "production" ? {
+  connectionString: getDatabaseUri(),
+  ssl: {
+    rejectUnauthorized: false
+  }
+} : dbConfig);
+
+async function testDbConnection() {
+  try {
+    console.log("Attempting to connect to the database...");
+    const result = await db.query(`SELECT NOW();`);
+    console.log("Current Time:", result.rows[0]);
+  } catch (error) {
+    console.error("Error testing database connection:", error);
+  }
 }
 
-db.connect();
+db.connect().then(testDbConnection);
 
 module.exports = db;

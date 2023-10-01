cd531e699f6be8511686d4bd57400d23f3e6e374
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 21:56:09 -0400

    Updated Job Routes

--- frontend/src/components/Jobs/JobList.js
+++ frontend/src/components/Jobs/JobList.js
@@ -1,18 +1,30 @@
+// JobList.js
 import React, { useState, useEffect } from 'react';
+import api from '../../helpers/api'; // Assuming you have an API helper
 import JobCard from './JobCard';
 
 function JobList() {
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch jobs from the API and update the state
-    // For example:
-    // async function fetchJobs() {
-    //   const response = await api.getJobs(); // Assuming you have an api helper
-    //   setJobs(response);
-    // }
-    // fetchJobs();
+    async function fetchJobs() {
+      try {
+        const fetchedJobs = await api.getJobs(); // Assuming you have a getJobs method in your API helper
+        setJobs(fetchedJobs);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
   }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error loading jobs: {error.message}</div>;
 
   return (
     <div>

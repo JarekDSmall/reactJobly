2fb24fb954e985712b49a0186371d073613f6cbc
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 20:42:29 -0400

    Refactor CompanyCard to Support Navigation
    Modified CompanyCard.js to wrap company names with a Link component.
    Ensured that clicking a company name in the card navigates to the correct detail page.

--- frontend/src/components/Companies/CompanyCard.js
+++ frontend/src/components/Companies/CompanyCard.js
@@ -1,11 +1,16 @@
 // Purpose: Display basic information about a company.
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function CompanyCard({ company }) {
+  console.log("Company ID:", company.id); // Debugging line to check the company id
   return (
-    <div>
-      {/* Render basic information about the company */}
-    </div>
+      <Link to={`/companies/${company.id}`}>
+          <div className="company-card">
+              <h3>{company.name}</h3>
+              {company.logo && <img src={company.logo} alt={company.name} />}
+          </div>
+      </Link>
   );
 }
 

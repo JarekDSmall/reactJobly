f88f183fc07adf171c0fea77887c445b09579d5b
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 20:46:45 -0400

    upload companies.js which acts as a container or a parent component for individual company components, managing the data-fetching, state, and user interactions related to displaying a list of companies.

---
+++ frontend/src/components/Companies/Companies.js
@@ -0,0 +1,50 @@
+// Companies.js
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Import the Link component
+import api from '../../helpers/api';
+
+function Companies() {
+  const [companies, setCompanies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    async function fetchCompanies() {
+      try {
+        const companiesData = await api.getCompanies(); // Assuming you have a method to fetch all companies
+        setCompanies(companiesData);
+      } catch (err) {
+        console.error("Error fetching companies:", err);
+      }
+    }
+
+    fetchCompanies();
+  }, []);
+
+  const filteredCompanies = companies.filter(company => 
+    company.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Search for a company..." 
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+      <ul>
+        {filteredCompanies.map(company => (
+          <li key={company.handle}>
+            {/* Wrap the company name in a Link component */}
+            <Link to={`/companies/${company.handle}`}>
+              {company.name}
+            </Link>
+            {/* Add more details or links as needed */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Companies;

4bb1bb200d9e58381b37e2503be3c39a7ee02529
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 20:48:43 -0400

    Update JobList.js

--- frontend/src/components/Jobs/JobList.js
+++ frontend/src/components/Jobs/JobList.js
@@ -1,17 +1,24 @@
-// Purpose: Display a list of all jobs.
 import React, { useState, useEffect } from 'react';
-import JobCard from '../JobCard/JobCard';
+import JobCard from './JobCard';
 
 function JobList() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
     // Fetch jobs from the API and update the state
+    // For example:
+    // async function fetchJobs() {
+    //   const response = await api.getJobs(); // Assuming you have an api helper
+    //   setJobs(response);
+    // }
+    // fetchJobs();
   }, []);
 
   return (
     <div>
-      {/* Render a list of JobCard components */}
+      {jobs.map(job => (
+        <JobCard key={job.id} job={job} />
+      ))}
     </div>
   );
 }

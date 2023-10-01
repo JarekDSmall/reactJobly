126dda5935b567939139147f5d3324a61e226eca
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 21:55:39 -0400

    Updated Job Routes

--- frontend/src/components/Jobs/JobCard.js
+++ frontend/src/components/Jobs/JobCard.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import JoblyApi from '../../helpers/api';
 
-
 function JobCard({ job }) {
   const [applied, setApplied] = useState(false);
 
   const handleApply = async () => {
-    await JoblyApi.applyForJob(job.id);
-    setApplied(true);
+    try {
+      await JoblyApi.applyForJob(job.id);
+      setApplied(true);
+    } catch (error) {
+      console.error("Error applying for job:", error);
+      
+    }
   };
 
   return (
     <div className="JobCard">
       <h2>{job.title}</h2>
       <p>{job.companyName}</p>
+      <p>Salary: ${job.salary}</p>
+      <p>Equity: {job.equity}</p>
       <button onClick={handleApply} disabled={applied}>
         {applied ? 'Applied' : 'Apply'}
       </button>

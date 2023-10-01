db18216f6682412a4846180611e086bd2e798d56
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-30 21:59:17 -0400

    increased specificity of detail by adding jobs to it

--- frontend/src/components/Companies/CompanyDetail.js
+++ frontend/src/components/Companies/CompanyDetail.js
@@ -1,39 +1,44 @@
-// CompanyDetail.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../helpers/api';
+import JobCard from '../Jobs/JobCard';  // Import the JobCard component
 
 function CompanyDetail() {
   const { handle } = useParams();
   const [company, setCompany] = useState(null);
+  const [jobs, setJobs] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-      async function fetchCompany() {
-          try {
-              const companyData = await api.getCompany(handle); // Use handle here
-              console.log("Fetched company data:", companyData); // Debugging line
-              setCompany(companyData);
-          } catch (err) {
-              console.error("Error fetching company:", err);
-              setError(err);
-          }
+    async function fetchCompanyAndJobs() {
+      try {
+        const companyData = await api.getCompany(handle);
+        setCompany(companyData);
+        const companyJobs = await api.getJobsByCompany(handle); // Assuming you have a method to get jobs by company
+        setJobs(companyJobs);
+      } catch (err) {
+        console.error("Error fetching company:", err);
+        setError(err);
       }
+    }
 
-      fetchCompany();
-  }, [handle]); // Use handle in the dependency array
+    fetchCompanyAndJobs();
+  }, [handle]);
 
   if (error) return <div>Error: {error.message}</div>;
   if (!company) return <div>Loading...</div>;
 
   return (
-      <div>
-          <h2>{company.name}</h2>
-          <p>Description: {company.description}</p>
-          <p>Number of Employees: {company.numEmployees}</p>
-          {company.logoUrl && <img src={company.logoUrl} alt={`${company.name} logo`} />}
-          {/* Add more details as needed */}
-      </div>
+    <div>
+      <h2>{company.name}</h2>
+      <p>Description: {company.description}</p>
+      <p>Number of Employees: {company.numEmployees}</p>
+      {company.logoUrl && <img src={company.logoUrl} alt={`${company.name} logo`} />}
+      <h3>Jobs at {company.name}</h3>
+      {jobs.map(job => (
+        <JobCard key={job.id} job={job} />
+      ))}
+    </div>
   );
 }
 

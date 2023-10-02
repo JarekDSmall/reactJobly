11f1b2b9e67c0b076b55d1c7a0489ef212d87552
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-10-01 22:40:11 -0400

    Refactored LoginForm.js to use cookies for token storage
    Removed useTokenStorage hook dependency.
    Integrated js-cookie for setting the authentication token upon successful login.

--- frontend/src/components/User/LoginForm.js
+++ frontend/src/components/User/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../helpers/api';
+import Cookies from 'js-cookie'; // Importing js-cookie
 
 function LoginForm({ onLogin }) {
   const [formData, setFormData] = useState({
@@ -17,27 +18,24 @@ function LoginForm({ onLogin }) {
       ...formData,
       [name]: value,
     });
-    console.log("Updated formData:", formData);
   };
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
-      await api.login(formData);  // Pass the entire formData object
+      const receivedToken = await api.login(formData);
+      Cookies.set('token', receivedToken); // Set the token using js-cookie
       setMessage('Login successful! Redirecting to dashboard...');
       onLogin();
-      navigate('/Home');  // Redirect to the Home page after successful login
+      navigate('/Home');
     } catch (error) {
       console.error("Login Error:", error);
       setMessage('Login failed. Please try again.');
     } finally {
       setLoading(false);
     }
-};
-
-
+  };
 
   return (
     <form onSubmit={handleSubmit}>

58334370b43bcd60a0476bfba8b15f9f3d1db94f
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-10-02 14:10:58 -0400

    Update SignupForm.js with FormStyles.css

--- frontend/src/components/User/SignupForm.js
+++ frontend/src/components/User/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../helpers/api';
+import './FormStyles.css';
 
 function SignupForm() {
   const [formData, setFormData] = useState({
